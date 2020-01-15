55d609cad2a1142c6f16ce90a1da0cc0e9c4742a
xavier <466461917@qq.com>
2020-01-15 07:55:36 +0000

    随机拒绝

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 //把秒杀接口放在路由的最开头，是因为 Laravel 匹配路由是从上往下匹配的
 //遇到第一个满足条件的路由就返回，所以放在最开头可以节省掉很多匹配路由的资源消耗。
 //秒杀
-Route::post('seckill_orders', 'OrdersController@seckill')->name('seckill_orders.store');
+Route::post('seckill_orders', 'OrdersController@seckill')->name('seckill_orders.store')->middleware('random_drop:80');
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
 
